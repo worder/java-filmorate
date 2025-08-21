ff853a9485371d3b8e364a2e478ebf59e05ece0d
worder <worderrus@yandex.com>
2025-08-21 18:47:03 +0700

    feat: remove data annotation from entities, change log level

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -5,13 +5,12 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Value;
 
 import java.time.LocalDate;
+import java.util.Objects;
 import java.util.Set;
 
-@Data
 @Value
 @Builder(toBuilder = true)
 public class User {
@@ -29,4 +28,45 @@ public class User {
 
     @PastOrPresent(message = "Birthday date should not be in future")
     LocalDate birthday;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass() || this.id == null) {
+            return false;
+        }
+
+        User user = (User) o;
+        return Objects.equals(id, user.id)
+                && Objects.equals(friends, user.friends)
+                && Objects.equals(email, user.email)
+                && Objects.equals(login, user.login)
+                && Objects.equals(name, user.name)
+                && Objects.equals(birthday, user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(id);
+        result = 31 * result + Objects.hashCode(friends);
+        result = 31 * result + Objects.hashCode(email);
+        result = 31 * result + Objects.hashCode(login);
+        result = 31 * result + Objects.hashCode(name);
+        result = 31 * result + Objects.hashCode(birthday);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", friends=" + friends +
+                ", email='" + email + '\'' +
+                ", login='" + login + '\'' +
+                ", name='" + name + '\'' +
+                ", birthday=" + birthday +
+                '}';
+    }
 }
